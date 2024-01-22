cf6f48d65af0f2d58deacf1337f0cff9480d25a8
Jeremy Leggat <jleggat@asu.edu>
2024-01-22 15:52:32 -0700

    Move public template to render_rss function
    Update rssnewsticker.php

--- partials/rssnewsticker-public-display.php
+++ partials/rssnewsticker-public-display.php
@@ -14,22 +14,15 @@
 
 define('DONOTCACHEPAGE', true);
 header('Content-Type: '.feed_content_type('rss2').';charset='.get_option('blog_charset'), true);
-echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
+echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.">";
 do_action('rss_tag_pre', 'rss2');
 ?>
 
-<rss version="2.0"
-	xmlns:content="http://purl.org/rss/1.0/modules/content/"
-	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-	xmlns:dc="http://purl.org/dc/elements/1.1/"
-	xmlns:atom="http://www.w3.org/2005/Atom"
-	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
-	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
-	<?php do_action('rss2_ns'); ?>>
+<rss version="2.0" <?php do_action('rss2_ns'); ?>>
 <channel>
-	<title><?php bloginfo_rss('name'); ?> - <?php get_option($this->plugin_name . '_feed_name'); ?> - Feed</title>
+	<title><?php bloginfo_rss('name'); ?> - Ticker Feed</title>
 	<link><?php bloginfo_rss('url') ?></link>
-	<description><?php bloginfo_rss('description') ?></description>
+	<description>For Local use for news ticker</description>
 	<?php do_action('rss2_head'); ?>
 
 	<?php
