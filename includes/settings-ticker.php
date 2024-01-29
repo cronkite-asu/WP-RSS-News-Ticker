09b1f4b0a3023dc46d20d8cf5b5aa9e881a6159a
Jeremy Leggat <jleggat@asu.edu>
2024-01-29 16:30:35 -0700

    update js

--- includes/settings-ticker.php
+++ includes/settings-ticker.php
@@ -51,52 +51,6 @@ class SettingsTicker extends Settings {
 			wp_enqueue_script( 'wp-util' );
 			wp_enqueue_script( 'postbox' );
 		}
-		add_action( "admin_footer-{$page_hook_id}", [ $this, 'footer_scripts' ] );
-	}
-
-	/**
-	 * Footer Script Needed for Repeater input:
-	 * - Meta Box Toggle.
-	 * - Spinner for Saving Option.
-	 * - Reset Settings Confirmation
-	 * @since 0.1.0
-	 */
-	public function footer_scripts(){
-		$page_hook_id = $this->get_hook_suffix();
-		$name = 'ticker_text';
-	?>
-
-	<script type="text/javascript">
-		//<![CDATA[
-		jQuery(document).ready( function($) {
-			'use strict';
-			// toggle
-			$('.if-js-closed').removeClass('if-js-closed').addClass('closed');
-			postboxes.add_postbox_toggles( '<?php echo $page_hook_id; ?>' );
-			// display spinner
-			$('<?php echo esc_attr( '#' . $this->id . '-form' ); ?>').submit( function(){
-				$('#publishing-action .spinner').addClass('is-active');
-			});
-			// confirm before reset
-			$('#delete-action .submitdelete').on('click', function() {
-				return confirm('Are you sure want to do this?');
-			});
-			$(function(){
-				$("#field_data_add").on( 'click', function(e){
-					e.preventDefault();
-					var template = wp.template('repeater'),
-						html = template();
-					$("#field_data").append( html );
-				});
-				$( document ).on( 'click', '.field-data-remove', function(e){
-					e.preventDefault();
-					$(this).parent().remove();
-				});
-			});
-		});
-		//]]>
-	</script>
-	<?php
 	}
 
 	/**
@@ -108,6 +62,7 @@ class SettingsTicker extends Settings {
 	 */
 	public function render_array( $args ) {
 
+		$page_hook_id = $this->get_hook_suffix();
 		$class = ! empty( $args['class'] ) ? $args['class'] : '';
 		$default = ! empty( $args['default'] ) ? $args['default'] : [""];
 		$field_data = $this->get_option( $args['name'], $default );
@@ -115,12 +70,15 @@ class SettingsTicker extends Settings {
 		error_log($this->get_option_key( $args['name'] ));
 
 		/* Repeater Text Input */
+		if ( ! empty( $args['description'] ) ) {
 		?>
 
-		<label for="field_data">
-			<strong><?php echo esc_attr( $args['title'] ); ?></strong>
+		<label for="<?php echo esc_attr( $args['name'] ); ?>-field_data">
+			<strong><?php echo esc_attr( $args['description'] ); ?></strong>
 		</label>
-		<div id="field_data">
+		<?php } ?>
+
+		<div id="<?php echo esc_attr( $args['name'] ); ?>-field_data">
 			<?php foreach( $field_data as $i => $value ) { ?>
 			<div class="field-group">
 				<input type="text" class="<?php echo esc_attr( $class ); ?>" name="<?php echo esc_attr( $this->get_option_key( $args['name'] ) ); ?>[<?php echo $i; ?>]" value="<?php echo $value; ?>" />
@@ -128,13 +86,34 @@ class SettingsTicker extends Settings {
 			</div>
 			<?php } ?>
 		</div>
-		<button type="button" id="field_data_add" class="button button-primary">Add</button>
+		<button type="button" class="button button-primary field-data-add">Add</button>
 
-		<script type="text/html" id="tmpl-repeater">
+		<script type="text/html" id="tmpl-<?php echo esc_attr( $args['name'] ); ?>-repeater">
 			<div class="field-group">
 				<input type="text" class="<?php echo esc_attr( $class ); ?>" name="<?php echo esc_attr( $this->get_option_key( $args['name'] ) ); ?>[]" value="" />
 				<button type="button" class="button button-secondary field-data-remove">X</button>
 			</div>
+		</script>
+
+		<script type="text/javascript">
+			//<![CDATA[
+			jQuery(document).ready( function($) {
+				'use strict';
+				$(function(){
+					var $fieldData = $('#<?php echo esc_attr( $args['name'] ); ?>-field_data');
+					$fieldData.next(".field-data-add").on( 'click', function(e){
+						e.preventDefault();
+						var template = wp.template('<?php echo esc_attr( $args['name'] ); ?>-repeater'),
+							html = template();
+						$fieldData.append( html );
+					});
+					$fieldData.on( 'click', '.field-data-remove', function(e){
+						e.preventDefault();
+						$(this).parent().remove();
+					});
+				});
+			});
+			//]]>
 		</script>
 	<?php
 	}
