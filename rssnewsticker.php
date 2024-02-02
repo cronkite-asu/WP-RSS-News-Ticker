bc6de6321b2fff0c9001a24ada858ea5a812c5c9
Jeremy Leggat <jleggat@asu.edu>
2024-02-01 17:52:53 -0700

    Move base plugin class
    Update rssnewsticker.php

--- rssnewsticker.php
+++ rssnewsticker.php
@@ -15,13 +15,7 @@
  * Text Domain:       rssnewsticker
  */
 
-namespace Rssnewsticker;
-include_once 'includes/class-settings.php';
-include_once 'includes/class-settings-page.php';
-include_once 'includes/class-settings-ticker.php';
-include_once 'includes/class-remote.php';
-include_once 'includes/class-remote-json.php';
-include_once 'includes/class-remote-ap-headlines.php';
+namespace ASU\CJS\Rssnewsticker;
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
@@ -35,176 +29,11 @@ if ( ! defined( 'WPINC' ) ) {
  */
 define( 'RSSNEWSTICKER_VERSION', '1.0.0' );
 
-
-class Rssnewsticker {
-
-	/**
-	 * The unique identifier of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-	 */
-	protected $plugin_name;
-
-	/**
-	 * The current version of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $version    The current version of the plugin.
-	 */
-	protected $version;
-
-	/**
-	 * Settings
-	 * @var [type]
-	 */
-	protected $settings;
-
-	/**
-	 * Settings
-	 * @var [type]
-	 */
-	protected $page;
-
-	/**
-	 * TIcker
-	 * @var [type]
-	 */
-	protected $ticker;
-
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function __construct() {
-		if ( defined( 'RSSNEWSTICKER_VERSION' ) ) {
-			$this->version = RSSNEWSTICKER_VERSION;
-		} else {
-			$this->version = '1.0.0';
-		}
-		$this->plugin_name = 'rssnewsticker';
-		$this->settings = new SettingsPage();
-		$this->ticker = new SettingsTicker();
-
-		$this->define_admin_hooks();
-		$this->define_public_hooks();
-
-	}
-
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_admin_hooks() {
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		add_action('init', array( $this, 'add_rss_feed'));
-	}
-
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The name of the plugin.
-	 */
-	public function get_plugin_name() {
-		return $this->plugin_name;
-	}
-
-	/**
-	 * Retrieve the version number of the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The version number of the plugin.
-	 */
-	public function get_version() {
-		return $this->version;
-	}
-
-	public function add_rss_feed() {
-		add_feed($this->settings->get_option('feed_name'), array( $this, 'render_rss_feed' ));
-	}
-
-	public function render_rss_feed () {
-		$schoolnews = $this->fetch_local_headlines();
-		$apnews = $this->fetch_ap_headlines();
-		$lines = array_merge($schoolnews, $apnews);
-
-		define('DONOTCACHEPAGE', true);
-		header('Content-Type: application/rss+xml');
-		echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.">";
-		do_action('rss_tag_pre', 'rss2');
-?>
-
-<rss version="2.0" <?php do_action('rss2_ns'); ?>>
-	<channel>
-		<title><?php bloginfo_rss('name'); ?> - Ticker Feed</title>
-		<link><?php bloginfo_rss('url') ?></link>
-		<description><?php bloginfo_rss('description') ?></description>
-<?php		do_action('rss2_head'); ?>
-
-<?php
-		foreach ($lines as $line) {
-			$line = sanitize_text_field($line);
-?>
-		<item>
-			<description><![CDATA[<?php echo wp_filter_nohtml_kses($line) ?>]]></description>
-<?php			do_action('rss2_item'); ?>
-		</item>
-<?php		}; ?>
-	</channel>
-</rss>
-<?php
-	}
-
-	public function fetch_local_headlines() {
-		$text = $this->ticker->get_option('ticker_text');
-
-		return $text;
-	}
-
-	public function fetch_ap_headlines() {
-		$enabled = $this->settings->get_option('ap_enable');
-		$productid = $this->settings->get_option('ap_productid');
-		$page_size = $this->settings->get_option('ap_page_size');
-		$api_key = $this->settings->get_option('ap_api_key');
-		$pre_feed = $this->settings->get_option('ap_pre_feed');
-
-		$headlines = [];
-
-		if ($enabled === 1) {
-			$remote_request = new RemoteAPHeadlines( $productid, $api_key, $page_size );
-			$remote_request->run();
-
-			$headlines = $remote_request->get_ap_headlines();
-
-			array_unshift($headlines, $pre_feed);
-		}
-		return $headlines;
-	}
-
-}
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-rssnewsticker.php';
 
 /**
  * Begins execution of the plugin.
@@ -218,6 +47,7 @@ class Rssnewsticker {
 function run_rssnewsticker() {
 
 	$plugin = new Rssnewsticker();
+	$plugin->run();
 
 }
 run_rssnewsticker();
