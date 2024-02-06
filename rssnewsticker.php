61408e5699cf66e88080671dddf0599c40f420db
Jeremy Leggat <jleggat@asu.edu>
2024-02-05 18:30:14 -0700

    Add GitHub Plugin URI

--- rssnewsticker.php
+++ rssnewsticker.php
@@ -13,6 +13,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       rssnewsticker
+ * GitHub Plugin URI: https://github.com/cronkite-asu/WP-RSS-News-Ticker
  */
 
 namespace ASU\CSJ\Rssnewsticker;
